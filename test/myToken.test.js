227bf4d516667cfb241375f791b3cddd4e07f03b
zacklee4j <zacklee4j@gmail.com>
2025-07-24 22:45:30 +0800

    Update myToken.test.js
    comment add new testcases

--- test/myToken.test.js
+++ test/myToken.test.js
@@ -68,5 +68,55 @@ describe("MyToken", function () {
         .to.emit(token, "Transfer")
         .withArgs(owner.address, addr1.address, total_balance);
     });
+    it('应返回正确的元数据',async function (){
+      const {token} = await loadFixture(deployTokenFixture);
+      expect(await token.name()).to.equal('MyToken');
+      expect(await token.symbol()).to.equal("MTK");
+      expect(await token.decimals()).to.equal(18);
+    });
+    it("非owner账户转账给另一非owner账户成功", async function(){
+      const {token,owner,addr1,addr2} = await loadFixture(deployTokenFixture);
+      const trans_balance = 1n * 10n ** 18n;
+      const total_balance = 100n * 10n ** 18n;
+      await token.transfer(addr1.address,trans_balance);
+      expect(await token.balanceOf(owner.address)).to.equal(total_balance - trans_balance);
+      expect(await token.balanceOf(addr1.address)).to.equal(trans_balance);
+      await token.connect(addr1).transfer(addr2.address,trans_balance);
+      expect(await token.balanceOf(addr1.address)).to.equal(0n);
+      expect(await token.balanceOf(addr2.address)).to.equal(trans_balance);
+    })
+    it("授权账户使用指定数量的代币成功", async function(){
+      const {token,owner,addr1,addr2} = await loadFixture(deployTokenFixture);
+      const trans_balance = 1n * 10n ** 18n;
+      const approve_balance = 10n * 10n ** 18n;
+      const total_balance = 100n * 10n ** 18n;
+      //console.log(0);
+      const init_appro_balance = await token.allowance(owner.address,addr1.address);
+      expect(init_appro_balance).to.equal(0n);
+      //console.log(1);
+      await token.connect(owner).approve(addr1.address, approve_balance);
+      expect(await token.balanceOf(owner.address)).to.equal(total_balance);
+      expect(await token.allowance(owner.address,addr1.address)).to.equal(approve_balance);
+      //console.log(2);
+      await token.connect(addr1).transferFrom(owner.address,addr2.address,trans_balance);
+      expect(await token.allowance(owner.address,addr1.address)).to.equal(approve_balance -trans_balance );
+      expect(await token.balanceOf(addr2.address)).to.equal(trans_balance);
+    })
+    it("授权交易额度不足时，回滚交易",async function (){
+      const {token,owner,addr1,addr2} = await loadFixture(deployTokenFixture);
+      const smallAmount = 50n * 10n ** 18n;
+      const largeAmount = 1000n * 10n ** 18n;
+       // 授权小额额度
+      await token.connect(owner).approve(addr1.address, smallAmount);
+      // 尝试转账超过授权额度
+      await expect(
+        token.connect(addr1).transferFrom(
+          owner.address,
+          addr2.address,
+          largeAmount
+        )
+      ).to.be.revertedWithCustomError(token, "ERC20InsufficientAllowance");
+
+    });
   });
   
