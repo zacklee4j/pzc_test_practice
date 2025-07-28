76ec7430e1cbe20afaa14158a245e3871e92a76d
zacklee4j <zacklee4j@gmail.com>
2025-07-28 22:31:58 +0800

    Update myToken.test.js
    Move the parameters out of the use cases and group them together.

--- test/myToken.test.js
+++ test/myToken.test.js
@@ -4,8 +4,17 @@ const {
   loadFixture, 
   time 
 } = require('@nomicfoundation/hardhat-toolbox/network-helpers');
-const { bigint } = require('hardhat/internal/core/params/argumentTypes');
 
+const myTokenTestData = {
+  init_token:100,
+  trans_balance : 1n * 10n ** 18n,
+  total_balance : 100n * 10n ** 18n,
+  over_balance : 1000n * 10n ** 18n,
+  mint_balance : 100n * 10n ** 18n,
+  approve_balance : 10n * 10n ** 18n,
+  smallAmount : 50n * 10n ** 18n,
+  largeAmount : 1000n * 10n ** 18n
+}
 
 describe("MyToken", function () {
     // 部署合约的fixture
@@ -13,7 +22,7 @@ describe("MyToken", function () {
       const [owner, addr1, addr2] = await ethers.getSigners();
       
       const Token = await ethers.getContractFactory("MyToken");
-      const token = await Token.deploy(100); // 100万初始供应
+      const token = await Token.deploy(myTokenTestData.init_token); // 100万初始供应
       
       return { token, owner, addr1, addr2 };
     }
@@ -21,52 +30,45 @@ describe("MyToken", function () {
     it("应该正确设置初始供应量", async function () {
       const { token, owner } = await loadFixture(deployTokenFixture);
       // console.log(await token.totalSupply());
-      const exp_value= 100n * 10n ** 18n;
-      expect(BigInt(await token.totalSupply())).to.equal(exp_value);
-      expect(await token.balanceOf(owner.address)).to.equal(exp_value);
+      expect(BigInt(await token.totalSupply())).to.equal(myTokenTestData.total_balance);
+      expect(await token.balanceOf(owner.address)).to.equal(myTokenTestData.total_balance);
     });
   
     it("应该允许代币转账", async function () {
       const { token, owner, addr1 } = await loadFixture(deployTokenFixture);
-      const trans_balance = 1n * 10n ** 18n;
-      const total_balance = 100n * 10n ** 18n;
       // 转账100代币
-      await token.transfer(addr1.address, trans_balance );
-      
+      await token.transfer(addr1.address, myTokenTestData.trans_balance );
       expect(await token.balanceOf(owner.address)).to.equal(
-        total_balance - trans_balance
+        myTokenTestData.total_balance - myTokenTestData.trans_balance
       );
-      expect(await token.balanceOf(addr1.address)).to.equal(trans_balance);
+      expect(await token.balanceOf(addr1.address)).to.equal(myTokenTestData.trans_balance);
     });
   
     it("应该拒绝超过余额的转账", async function () {
       const { token, addr1 } = await loadFixture(deployTokenFixture);
-      const over_balance = 1000n * 10n ** 18n;
       // 尝试转超过余额的代币
       await expect(
-        token.connect(addr1).transfer(addr1.address, over_balance)
+        token.connect(addr1).transfer(addr1.address, myTokenTestData.over_balance)
       ).to.be.revertedWithCustomError(token, "ERC20InsufficientBalance");
     });
   
     it("只有所有者可以铸造新代币", async function () {
       const { token, owner, addr1 } = await loadFixture(deployTokenFixture);
-      const total_balance = 100n * 10n ** 18n;
       // 所有者可以铸造
-      await token.mint(owner.address, total_balance);
-      expect(await token.totalSupply()).to.equal(total_balance * 2n);
+      await token.mint(owner.address, myTokenTestData.mint_balance);
+      expect(await token.totalSupply()).to.equal(myTokenTestData.mint_balance * 2n);
       
       // 非所有者尝试铸造
       await expect(
-        token.connect(addr1).mint(addr1.address, total_balance)
+        token.connect(addr1).mint(addr1.address, myTokenTestData.mint_balance)
       ).to.be.revertedWithCustomError(token, "OwnableUnauthorizedAccount");
     });
   
     it("应该正确触发Transfer事件", async function () {
       const { token, owner, addr1 } = await loadFixture(deployTokenFixture);
-      const total_balance = 100n * 10n ** 18n;
-      await expect(token.transfer(addr1.address, total_balance))
+      await expect(token.transfer(addr1.address, myTokenTestData.total_balance))
         .to.emit(token, "Transfer")
-        .withArgs(owner.address, addr1.address, total_balance);
+        .withArgs(owner.address, addr1.address, myTokenTestData.total_balance);
     });
     it('应返回正确的元数据',async function (){
       const {token} = await loadFixture(deployTokenFixture);
@@ -76,14 +78,12 @@ describe("MyToken", function () {
     });
     it("非owner账户转账给另一非owner账户成功", async function(){
       const {token,owner,addr1,addr2} = await loadFixture(deployTokenFixture);
-      const trans_balance = 1n * 10n ** 18n;
-      const total_balance = 100n * 10n ** 18n;
-      await token.transfer(addr1.address,trans_balance);
-      expect(await token.balanceOf(owner.address)).to.equal(total_balance - trans_balance);
-      expect(await token.balanceOf(addr1.address)).to.equal(trans_balance);
-      await token.connect(addr1).transfer(addr2.address,trans_balance);
+      await token.transfer(addr1.address,myTokenTestData.trans_balance);
+      expect(await token.balanceOf(owner.address)).to.equal(myTokenTestData.total_balance - myTokenTestData.trans_balance);
+      expect(await token.balanceOf(addr1.address)).to.equal(myTokenTestData.trans_balance);
+      await token.connect(addr1).transfer(addr2.address,myTokenTestData.trans_balance);
       expect(await token.balanceOf(addr1.address)).to.equal(0n);
-      expect(await token.balanceOf(addr2.address)).to.equal(trans_balance);
+      expect(await token.balanceOf(addr2.address)).to.equal(myTokenTestData.trans_balance);
     })
     it("授权账户使用指定数量的代币成功", async function(){
       const {token,owner,addr1,addr2} = await loadFixture(deployTokenFixture);
@@ -104,16 +104,14 @@ describe("MyToken", function () {
     })
     it("授权交易额度不足时，回滚交易",async function (){
       const {token,owner,addr1,addr2} = await loadFixture(deployTokenFixture);
-      const smallAmount = 50n * 10n ** 18n;
-      const largeAmount = 1000n * 10n ** 18n;
        // 授权小额额度
-      await token.connect(owner).approve(addr1.address, smallAmount);
+      await token.connect(owner).approve(addr1.address, myTokenTestData.smallAmount);
       // 尝试转账超过授权额度
       await expect(
         token.connect(addr1).transferFrom(
           owner.address,
           addr2.address,
-          largeAmount
+          myTokenTestData.largeAmount
         )
       ).to.be.revertedWithCustomError(token, "ERC20InsufficientAllowance");
 
